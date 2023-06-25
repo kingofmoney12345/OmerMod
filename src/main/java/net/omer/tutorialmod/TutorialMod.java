b1c3b8100950080fb63f83aa6c16540d9b80e65f
kingofmoney12345 <omer.oliver@gmail.com>
2023-06-25 15:11:51 +0300

    Added items and blocks

--- src/main/java/net/omer/tutorialmod/TutorialMod.java
+++ src/main/java/net/omer/tutorialmod/TutorialMod.java
@@ -8,6 +8,8 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.omer.tutorialmod.block.ModBlocks;
+import net.omer.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -19,6 +21,9 @@ public class TutorialMod
     public TutorialMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
